652c67bd3fc3e337b7682c2e96ac42eb11086f3e
Isrel Murillo <isramuri400@gmail.com>
2023-01-27 21:44:17 -0500

    TRABAJO FINAL

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ const footerContainer = document.querySelector("#footer")
 const headerTitulo = document.querySelector(".header-titulo")
 const headerSaludo = document.querySelector(".header-saludo")
 
+const formUsuarioNuevo = document.querySelector(".registro")
+const inputNuevoNombre = document.querySelector("#new-name")
+const inputNuevoUsuario = document.querySelector("#new-user")
+const inputNuevoPassword = document.querySelector("#new-password")
 
 
 
@@ -47,7 +51,7 @@ const obtenerInfoLocalStorage = (key) => {
     return JSON.parse(localStorage.getItem(key))
 } 
 
-
+// VALIDACION DE USUARIOS YA INSCRITOS, hay una serie de ellos seteado en el archivo usuarios.js
 
 formLogin.onsubmit = (event) => {
     event.preventDefault()
@@ -59,8 +63,22 @@ formLogin.onsubmit = (event) => {
             headerTitulo.style.display = "none"
             headerSaludo.style.display = "flex"
             swal(`Bienvenido ${usuarios[i].name}`)           
+        }else if (inputUser.value === usuarios[i].user && inputPassword.value !== usuarios[i].password){
+            swal(`Contraseña Incorrecta`)
+            loginFailed.style.display = "block"
+            loginFailed.style.color = "red"
+            inputUser.style.border = "1px solid red"
+            inputPassword.style.border = "1px solid red"
+        }
+        else if (inputUser.value !== usuarios[i].user && inputPassword.value === usuarios[i].password){
+            swal(`Usuario Incorrecto`)
+            loginFailed.style.display = "block"
+            loginFailed.style.color = "red"
+            inputUser.style.border = "1px solid red"
+            inputPassword.style.border = "1px solid red"
         }else {
             loginFailed.style.display = "block"
+            loginFailed.style.color = "red"
             inputUser.style.border = "1px solid red"
             inputPassword.style.border = "1px solid red"
         }
@@ -91,6 +109,76 @@ salir.onclick = () => {
     localStorage.removeItem("login")
     validarLogin(obtenerInfoLocalStorage("login"))
     formLogin.reset()
+}
+
+
+//VALIDACION DE NUEVOS USUARIOS 
+// USUARIO DEBE DE TENER UN @ 
+// CONTRASEÑA DEBE DE SER ALFANUMERICO Y TENER MINIMO 8 DIGITOS 
+
+
+formUsuarioNuevo.onsubmit = (event) =>{
+    event.preventDefault()
+
+        let detectarArroba = 0    
+        let contadorPassword = 0 
+        let detectorPassword = 0
+
+        let name = inputNuevoNombre.value
+        let user = inputNuevoUsuario.value
+        let password = inputNuevoPassword.value
+
+        for ( let i = 0; i < user.length; i++){             
+            if ( user[i].match(/[@]/)) {
+                detectarArroba = detectarArroba + 1}
+        }
+    
+
+        for ( let i = 0; i < password.length; i++){        
+            contadorPassword++
+        }
+
+        for ( let i = 0; i < password.length; i++){         
+            if (password[i].match(/^[0-9]+$/)){
+                detectorPassword++
+            }
+        }
+
+
+        if (detectarArroba > 0 && contadorPassword >= 8 && detectorPassword >0){
+            swal(`Inscripcion Exitosa`) 
+        }
+        else if (detectarArroba <= 0 && contadorPassword >= 8 && detectorPassword >0){
+            swal(`Usuario inválido, debe de tener @, ingrese un nuevo email `) 
+        }
+        else if (detectarArroba > 0 && contadorPassword < 8 && detectorPassword >0){
+            swal(`Contraseña inválida, tiene menos de 8 caracteres.`)
+        }
+        else if (detectarArroba > 0 && contadorPassword >= 8 && detectorPassword <= 0){
+            swal(`Contraseña inválida, no tines caracteres numericos`)
+        }
+        else if (detectarArroba > 0 && contadorPassword < 8 && detectorPassword <=0){
+            swal(`Contraseña inválida, no tines caracteres alfanumericos y tiene menos de 8 caracteres`)
+        }
+        else{
+            swal(`Usuario Invalido`)
+        }
+
+        fetch("https://63d485980e7ae91a009e7517.mockapi.io/usuariosNuevos", {
+            method: "POST",
+            body: JSON.stringify({
+                user:  `${user}`,
+                password: `${password}`,
+                name: `${name}`
+            }),
+            headers: {
+                "Content-Type":"application/json"
+            }
+        })
+        .then( res => res.json())
+        .then( data => console.log(data))
+
+
 }
 
 
--- usuarios.js
+++ usuarios.js
@@ -1,26 +1,26 @@
 const usuarios = [{
-    user: "israel",
+    user: "israel@gmail.com",
     password: "israel123",
     name: " Israel Murillo"
 },
 {
-    user: "jose",
-    password: "123",
+    user: "jose@gmail.com",
+    password: "Hola1234",
     name: " Jose Francisco"
 },
 {
-    user: "gustavo",
+    user: "gustavo@gmail.com",
     password: "gustavo123",
     name: "Gustavo Otalvaro"
 },
 {
-    user: "volar",
+    user: "volar@volar.com",
     password: "Hangar29",
     name: "Volar Colombia"
 },
 {
-    user: "daniela",
-    password: "danny",
+    user: "daniela@gmail.com",
+    password: "danny1234",
     name: "Daniela Sierra"
 }
 ]
